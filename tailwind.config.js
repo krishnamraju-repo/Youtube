90ecdab64c60d9b12b31048b099893eb351a9d14
krishnamraju-repo <vsrkraju21@gmail.com>
2024-07-23 06:49:43 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.js"],
+  // content: ["./src/**/*.{js,jsx,html}"] if you have more files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
